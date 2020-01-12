3cdf282d2c88f73522b48c74719f47e416696deb
Andrew Cobb <andrew.cobb@gmail.com>
2020-01-12 15:52:15 -0700

    add documentation to linux-spawn.c

--- Sources/linuxSpawn/linux-spawn.c
+++ Sources/linuxSpawn/linux-spawn.c
@@ -12,14 +12,31 @@
 
 #include "include/linux-spawn.h"
 
-// I sometimes hate linux...
-int spawn(pid_t *pidp,
-          const char *file,
-          char *const argv[],
-          char *const envp[],
-          int32_t *const fdMap,
-          _Bool pathResolve)
-{
+/// spawn a process in a similar manner to posix_spawn using options
+/// POSIX_SPAWN_CLOEXEC_DEFAULT and POSIX_SPAWN_START_SUSPENDED, neither of
+/// which are available on linux.
+///  - Parameter command: name of executable. Either the path to an executable,
+///    or will be looked up in PATH. (Passed directly to `execvpe`)
+///  - Parameter argv: null terminated list of arguments to pass to command.
+///    (Passed directly to `execvpe`)
+///  - Parameter envp: null terminated list of all environment variables to pass
+///    to command. (Passed directly to `execvpe`)
+///  - Parameter fdMap: a list of mappings from parent FDs to be inherited by child
+///    FDs. Any FD not listed as a dst will be closed for the child. The list
+///    must be -1 terminated, and in the format:
+///    `{ src_0, dst_0, src_1, dst_1, ..., src_n, dst_n, -1 }`
+///  - Returns: 0 if successful, an error code if an operation failed.
+///  - Parameter child_pid_out: non-null, pid of child process will be written
+///    to this pointer if successful.
+int spawn(
+    const char *command,
+    char *const argv[],
+    char *const envp[],
+    int32_t *const fdMap,
+    pid_t *child_pid_out
+) {
+    // First, create a table for fast lookup if an FD is a source and/or a
+    // destination for dup
     int fdlimit = (int)sysconf(_SC_OPEN_MAX);
     const int SRC = 1;
     const int DST = 2;
@@ -29,19 +46,25 @@ int spawn(pid_t *pidp,
         mentionedFds[p[0]] |= SRC;
         mentionedFds[p[1]] |= DST;
     }
+
     int pid;
     int pipeFds[2];
-    // create a pipe so that the child can report to the parent on failure. If it's closed silently, it's a successful exec.
+    // create a pipe so that the child can report to the parent on failure. If
+    // it's closed silently, it's a successful exec. If not, it will send an
+    // error code.
     if (pipe2(pipeFds, O_CLOEXEC) != 0) {
         return errno;
     }
+
     if (!(pid = fork())) {
-        // make sure we aren't overwriting our pipe's fd by duping it
+        // In child process. Prepare state for exec.
+
+        // Find an FD to store our pipe in that won't interfere with fdMap
         int writePipe = -1;
         for (int i = 0; i < fdlimit; i++) {
             if (!mentionedFds[i]) {
                 if (i == pipeFds[i]) {
-                    // good fd, nothing to do
+                    // pipe picked a good fd, nothing to do
                     writePipe = i;
                 } else if ((writePipe = dup3(pipeFds[1], i, O_CLOEXEC)) < 0) {
                     goto err;
@@ -50,31 +73,43 @@ int spawn(pid_t *pidp,
             }
         }
         if (writePipe < 0) {
+            // All file descriptors seem to be in use, not one to spare for some
+            // bookkeeping :(
             errno = EMFILE;
             goto err;
         }
+        // do the actual fd remapping
         for (int32_t *p = fdMap; *p != -1; p += 2) {
             if (dup2(p[0], p[1]) < 0) {
                 goto err;
             }
         }
         for (int i = 0; i < fdlimit; i++) {
+            // close all the FDs we don't need, even ones that aren't open
+            // (faster than checking which ones are open I read somewhere on the
+            // internet)
             if (!(mentionedFds[i] & DST) && i != writePipe) {
                 close(i);
             }
         }
-        execvpe(file, argv, envp);
+        // All good! If this returns, exec failed with an error.
+        execvpe(command, argv, envp);
       err:;
         int err = errno;
         write(writePipe, &err, sizeof(err));
         exit(err);
     }
+    // parent code
+
+    // close our copy of the pipe's write end
     close(pipeFds[1]);
     int ret = 0;
     if (pid < 0) {
+        // fork failed
         ret = pid;
     } else if (read(pipeFds[0], &ret, sizeof(ret)) == 0) {
-        *pidp = pid;
+        // pipe was closed silently, fork and exec succeeded
+        *child_pid_out = pid;
     }
     close(pipeFds[0]);
     return ret;
