62750ea0d33e71e025020bfd505cd6a464a9a019
Andrew Cobb <andrew.cobb@gmail.com>
2020-07-20 08:21:08 -0600

    chore: make lint config nicer

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,17 @@
-const typeEnum = require('@commitlint/config-angular-type-enum');
+const typeEnum = [
+  "feat", // A new feature
+  "fix", // A bug fix
+  "docs", // Documentation only changes
+  "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+  "refactor", // A code change that neither fixes a bug nor adds a feature
+  "perf", // A code change that improves performance
+  "test", // Adding missing or correcting existing tests
+  "chore", // Changes to the build process or auxiliary tools and libraries such as documentation generation
+]
 
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-enum': [2, 'always', typeEnum.value().concat(['chore'])],
+    'type-enum': [2, 'always', typeEnum],
   },
 };
